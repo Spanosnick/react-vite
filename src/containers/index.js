929c62456a0b8159c5f0205ad0eeef94e73c3e68
Spanos <nickspanosf1@gmail.com>
2024-12-11 21:00:02 +0200

    folders structure

---
+++ src/containers/index.js
@@ -0,0 +1,4 @@
+export { default as Blog } from './blog/Blog.jsx';
+export { default as Features } from './features/Features.jsx';
+export { default as Footer } from './footer/Footer.jsx';
+
---
+++ src/containers/whatGPT3/Whatgpt3.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './whatgpt3.css';
+
+export default function Whatgpt3() {
+    return (<div>
+    
+    </div>);
+}
